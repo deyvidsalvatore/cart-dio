84f3fe68e047ec8f668350f93cb1c13e4ddd3dd4
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-04-17 23:44:54 -0300

    Methods and Item.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import createItem from "./services/item.js";
+
 const cart = [];
+
+console.log("Welcome to the your Shopee Cart!");
+
+const item1 =  await createItem("Action Figure Yasuo", 30.99, 1);
+const item2 = await createItem("Capa iPhone 14", 10.00, 3);
+
+console.log(item2.subtotal());
